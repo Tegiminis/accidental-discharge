aac7efb76989487dcc8bd132f394a23e6caced67
Tegiminis <dragonmaw@gmail.com>
2025-12-10 12:48:33 -0800

    discharge now properly rotates to surface normals

--- objects/discharge.gd
+++ objects/discharge.gd
@@ -1,5 +1,19 @@
 extends Area3D
 class_name Discharge
 
+@export var debug_persist : bool = true
+
 func _ready() -> void:
 	pass
+func _physics_process(delta: float) -> void:
+	var bodies = get_overlapping_bodies()
+	if not debug_persist:
+		queue_free()
+	if bodies:
+		var target = null
+		var last_dist = null
+		for body in bodies:
+			var dist = global_position.distance_to(body.global_position)
+			if last_dist == null: last_dist = dist
+			elif last_dist > dist: last_dist = dist
+		queue_free()
--- objects/weapon.gd
+++ objects/weapon.gd
@@ -9,12 +9,11 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(linear_velocity*delta)
 	if collision:
-		print(collision.get_collider())
 		var norm = collision.get_normal()
 		var pos = collision.get_position()
 	
-		var new_impact = on_hit.instantiate()
-		get_tree().get_root().add_child(new_impact)
-		new_impact.position = pos
-		new_impact.rotation = norm
+		var discharge = on_hit.instantiate()
+		get_tree().get_root().add_child(discharge)
+		discharge.position = pos
+		discharge.look_at(to_global(norm))
 		queue_free()
